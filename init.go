529031e1a6ac3db73a929f9f0cf07804c9f6a34e
BeeAdmin <carchoufleurropebeesilly@gmail.com>
2015-12-13 14:32:17 -0500

    Update init.go

--- init.go
+++ init.go
@@ -1,8 +1,8 @@
 package sf
 
 import (
-	"github.com/go-gl/gl"
-	glfw "/home/malek/Go/src/github.com/go-gl/glfw3/v3.1/glfw"
+	"github.com/go-gl/gl/v3.3-core/gl"
+	glfw "github.com/go-gl/glfw3/v3.1/glfw"
 )
 
 func init() {
